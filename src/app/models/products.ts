a81562f475f8cbbcfb297088f3000fc2b5501fa7
Hope Ross <hope.ross@daveramsey.com>
2022-06-06 15:46:50 -0500

    first push

---
+++ src/app/models/products.ts
@@ -0,0 +1,8 @@
+export class Products {
+    id: number = 0;
+    productName: string = "";
+    productDescription: string = "";
+    productSize: string = "";
+    productPrice: number = 0;
+    productImageURL: string = "";
+}
